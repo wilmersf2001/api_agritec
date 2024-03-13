27aebc6282180f67c181aebb73c4157ddd7c5033
wilmersf2001 <wilmersf2001@users.noreply.github.com>
2024-03-13 11:37:13 -0500

    Actualizar toArray en ProductResource.php

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,16 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'nombre' => $this->nombre,
+            'descripcion' => $this->descripcion,
+            'precio' => $this->precio,
+            'cantidad' => $this->cantidad,
+            'ruta_imagen' => $this->ruta_imagen,
+            'user_id' => $this->user_id,
+            'category_id' => $this->category_id,
+            'created_at' => $this->created_at,
+        ];
     }
 }
